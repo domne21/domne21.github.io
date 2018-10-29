8d4e1829ddb6d64775a662d4c8addc91c15a9cc6
domne21 <43964249+domne21@users.noreply.github.com>
2018-10-29 20:58:23 +0200

    simple cursValutar

--- js/learn.js
+++ js/learn.js
@@ -10,9 +10,31 @@ var template = `Text cu ghilimele " sau '`;
 var skills = ["html", "css", "js"];
 
 console.debug("Auzi..? dar cum te cheama? -" + prenume);
+sayWelcome();
 
 prenume = "Dany";
 
 console.info("toate variabilele au fost initializate");
 console.log(nume);
 console.debug("Cum te cheama? -" + prenume);
+
+// function executCeva(mesaj){console.log("eu sunt" + mesaj)};
+
+function sayWelcome() {
+  console.info("Salut " + prenume + ",bine ai venit la noi!");
+}
+sayWelcome();
+
+// schimb valutar
+// operatori * / + -
+
+var cursEUR = 4.5;
+var myEUR = 20;
+var myRON = 0;
+var friendEUR = 10;
+var friendRON = 0;
+
+myRON = myEUR * cursEUR;
+console.info("Eu am " + myRON + " RON.");
+friendRON = friendEUR * cursEUR;
+console.info("Tu ai " + friendRON + " RON.");
