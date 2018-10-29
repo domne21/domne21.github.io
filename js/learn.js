e874789e28e8d8d83acb2b3bddcf9b35037fda91
domne21 <43964249+domne21@users.noreply.github.com>
2018-10-29 20:26:44 +0200

    add strig variables

--- js/learn.js
+++ js/learn.js
@@ -1,1 +1,18 @@
 console.info(0);
+
+var nume = "Domnescu";
+var prenume = "Daniel";
+var owner = "Daniel's";
+var msg = 'ai zis "Nick?"';
+var msg2 = 'Nu, am zis "Nicolae"';
+var template = `Text cu ghilimele " sau '`;
+
+var skills = ["html", "css", "js"];
+
+console.debug("Auzi..? dar cum te cheama? -" + prenume);
+
+prenume = "Dany";
+
+console.info("toate variabilele au fost initializate");
+console.log(nume);
+console.debug("Cum te cheama? -" + prenume);
