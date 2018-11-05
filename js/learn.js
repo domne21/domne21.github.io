2dfd13072313d7ac233342236af8a74afac12f6e
domne21 <43964249+domne21@users.noreply.github.com>
2018-11-05 19:55:18 +0200

    add getExtractFee, add global vars ATMFunds, funds

--- js/learn.js
+++ js/learn.js
@@ -6,7 +6,6 @@ var owner = "Daniel's";
 var msg = 'ai zis "Nick?"';
 var msg2 = 'Nu, am zis "Nicolae"';
 var template = `Text cu ghilimele " sau '`;
-
 var skills = ["html", "css", "js"];
 
 console.debug("Auzi..? dar cum te cheama? -" + prenume);
@@ -41,18 +40,40 @@ friendRON = friendEUR * cursEUR - (friendEUR * cursEUR * 1) / 100;
 console.info("Tu ai " + friendRON + " RON.");
 
 //retragere bancomat
+var ATMFunds = 5000;
+var funds = 5000;
 
-function extractFromATM(ammount) {
-  console.info("==== ==== ==== ====");
-  console.info("suma extrasa este: " + ammount);
+function getExtractFee(ammount) {
   var comision = ammount * 0.01;
   if (comision < 2.5) {
     comision = 2.5;
     console.warn("comision minim aplicat");
   }
+  return comision;
+}
+
+function extractFromATM(ammount) {
+  console.info("==== ==== ==== ====");
+  console.info("suma extrasa este: " + ammount);
+  var comision = getExtractFee(ammount);
+  var totalExtract = ammount + comision;
+
+  if (totalExtract > funds) {
+    console.error("Insufficient founds,");
+    return;
+  }
+  if (ATMFunds < totalExtract) {
+    console.error("ATM insufficient founds,");
+  }
+  funds = funds - totalExtract;
+
   console.info("comision aplicat: " + comision);
+  console.info("Sold curent: " + funds + " RON.");
   console.info("==== ==== ==== ====");
 }
 
-extractFromATM(1000);
+extractFromATM(1005); //nu e multiplu de 10
+extractFromATM(100000); // solduri insuficiente
+extractFromATM(3000);
+extractFromATM(3000);
 extractFromATM(100);
