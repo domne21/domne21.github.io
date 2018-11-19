2fdedda741e9f22b1735abbb853cdca1d3bc0738
Daniel Constantin Domnescu <domnescudaniel@gmail.com>
2018-11-19 20:02:28 +0200

    added education

--- src/index.js
+++ src/index.js
@@ -1,28 +1,37 @@
 function $(id) {
-   return document.getElementById(id);
+    return document.getElementById(id);
 }
 function hide(id) {
-    $(id).style.display ="none" ;
+    $(id).style.display = "none";
 }
-function show (id) {
-    $(id).style.display ="block" ;
+function show(id) {
+    $(id).style.display = "block";
 }
 function showSkillsPage() {
-    hide("home-page") ;
-    hide("languages-page") ;
-    show("skills-page");    
+    hide("home-page");
+    hide("languages-page");
+    hide("education-page");
+    show("skills-page");
+}
+function showEducationPage() {
+    hide("home-page");
+    hide("languages-page");
+    hide("skills-page");
+    show("education-page");
 }
 function showLanguagesPage() {
-    hide("home-page") ;
-    hide("skills-page");    
-    show("languages-page");    
+    hide("home-page");
+    hide("skills-page");
+    show("languages-page");
+    hide("education-page");
 }
 function showHomePage() {
-    hide("skills-page");    
-    hide("languages-page");    
-    show("home-page") ;
+    hide("skills-page");
+    hide("languages-page");
+    show("home-page");
+    hide("education-page");
 }
-
 $("home-menu").onclick = showHomePage;
 $("skills-menu").onclick = showSkillsPage;
+$("education-menu").onclick = showEducationPage;
 $("languages-menu").onclick = showLanguagesPage;
