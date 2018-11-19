8944476525202b7ca5405035147146e2f3714119
Daniel Constantin Domnescu <domnescudaniel@gmail.com>
2018-11-19 21:12:27 +0200

    up

--- src/index.js
+++ src/index.js
@@ -7,30 +7,30 @@ function hide(id) {
 function show(id) {
     $(id).style.display = "block";
 }
-function showSkillsPage() {
-    hide("home-page");
-    hide("languages-page");
-    hide("education-page");
-    show("skills-page");
-}
-function showEducationPage() {
-    hide("home-page");
-    hide("languages-page");
-    hide("skills-page");
-    show("education-page");
-}
-function showLanguagesPage() {
-    hide("home-page");
-    hide("skills-page");
-    show("languages-page");
-    hide("education-page");
-}
-function showHomePage() {
-    hide("skills-page");
-    hide("languages-page");
-    show("home-page");
-    hide("education-page");
-}
+// function showSkillsPage() {
+//     hide("home-page");
+//     hide("languages-page");
+//     hide("education-page");
+//     show("skills-page");
+// }
+// function showEducationPage() {
+//     hide("home-page");
+//     hide("languages-page");
+//     hide("skills-page");
+//     show("education-page");
+// }
+// function showLanguagesPage() {
+//     hide("home-page");
+//     hide("skills-page");
+//     show("languages-page");
+//     hide("education-page");
+// }
+// function showHomePage() {
+//     hide("skills-page");
+//     hide("languages-page");
+//     show("home-page");
+//     hide("education-page");
+// }
 // $("home-menu").onclick = showHomePage;
 // $("skills-menu").onclick = showSkillsPage;
 // $("education-menu").onclick = showEducationPage;
@@ -46,11 +46,9 @@ var links = document.querySelectorAll("#top-menu-bar a");
 for(var i = 0; i < links.length; i++) {
     // console.log(links[i].getAttribute("data-page"), links[i]);
     links[i].onclick = function() {
-        console.warn('hide all pages');
         hideAllPages();
         var page = this.getAttribute("data-page")
         show(page + "-page");
     };
 }
-
 show("home-page");
